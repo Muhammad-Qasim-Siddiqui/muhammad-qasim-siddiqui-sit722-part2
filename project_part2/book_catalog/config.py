e094baefbd2245850e7073862356fee252774db9
Muhammad-Qasim-Siddiqui <mqasim458.mq@gmail.com>
2024-09-19 06:26:46 +1000

    Added project_part2 files for SIT722 task

--- project_part2/book_catalog/config.py
+++ project_part2/book_catalog/config.py
@@ -1,6 +1,13 @@
 import os
 
 class Settings:
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    DATABASE_URL: str = os.getenv(
+        "DATABASE_URL", 
+        "postgresql://book_catalog_db_sit722_part_2_db_user:j0i1YQYQGPqf7tygbu7jl7Dc23Gk2N75@dpg-crlfifjtq21c73eeh8pg-a.oregon-postgres.render.com/book_catalog_db_sit722_part_2_db"
+    )
+
+    def __init__(self):
+        if not self.DATABASE_URL:
+            raise ValueError("DATABASE_URL environment variable is not set")
 
 settings = Settings()
--- project_part2/book_catalog/db.py
+++ project_part2/book_catalog/db.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from config import settings
 
+# Create engine using DATABASE_URL from the settings
 engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
